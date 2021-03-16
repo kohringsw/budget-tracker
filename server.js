d6b91316d82dbe29ed2f69d0c07acd5449d2916c
shelbykohring <71856110+kohringsw@users.noreply.github.com>
2021-03-15 21:04:35 -0400

    add console.log

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+console.log(MONGODB_URI);
 
 const app = express();
 
